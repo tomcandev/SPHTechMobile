226e64b61c8c320012e8985513ddb8acbd5076b5
Kycomputer555 <Kycomputer555@yahoo.com.vn>
2020-05-09 13:21:31 +0700

    Create Mobile Data Usage Module

---
+++ mobiledatausage/src/main/java/com/tomcandev/mobiledatausage/presentation/yearlydatausage/YearlyListFragment.kt
@@ -0,0 +1,32 @@
+package com.tomcandev.mobiledatausage.presentation.yearlydatausage
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.tomcandev.mobiledatausage.R
+
+class YearlyListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = YearlyListFragment()
+    }
+
+    private lateinit var viewModel: YearlyListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_yearly_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(YearlyListViewModel::class.java)
+    }
+
+}
---
+++ mobiledatausage/src/main/java/com/tomcandev/mobiledatausage/presentation/yearlydatausage/YearlyListViewModel.kt
@@ -0,0 +1,7 @@
+package com.tomcandev.mobiledatausage.presentation.yearlydatausage
+
+import androidx.lifecycle.ViewModel
+
+class YearlyListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
